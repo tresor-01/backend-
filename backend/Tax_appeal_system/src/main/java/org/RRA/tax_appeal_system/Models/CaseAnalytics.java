2af998813fbdff79c0051fff5c8beaa0e3ad2c93
tita <142999067+tresor-01@users.noreply.github.com>
2025-10-31 11:07:04 +0400

    Create CaseAnalytics.java

---
+++ backend/Tax_appeal_system/src/main/java/org/RRA/tax_appeal_system/Models/CaseAnalytics.java
@@ -0,0 +1,22 @@
+package org.RRA.tax_appeal_system.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "case_analytics")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CaseAnalytics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String date;
+    private int approved;
+    private int rejected;
+}
